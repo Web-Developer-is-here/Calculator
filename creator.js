d8212cb304afe48b2e771758562280cbe58127f9
Web-Developer-is-here <140488803+Web-Developer-is-here@users.noreply.github.com>
2024-01-31 16:40:02 +0530

    Update creator.js

--- creator.js
+++ creator.js
@@ -10,39 +10,33 @@ const data_Fetcher = async () => {
   
   const fresh_data = await raw_data.json();
   const creator = document.querySelector('div#creator');
-  
+  //console.log(fresh_data)
   const creator_heading = `${ fresh_data.creator_heading }`;
-  const creator_info = `${fresh_data.creator_para}  ${fresh_data.additional_info}`;
-  creator.innerHTML = creator_heading;
+//  console.log(creator_heading)
+  const creator_info = `${creator_heading} ${fresh_data.creator_para}  ${fresh_data.additional_info}`;
+  // console.log(creator_info)
+  // creator.innerHTML = creator_heading;
   
-  
-  
-  const typed = new Typed('#creator_para', {
+  const typed = new Typed('div#creator_para', {
       strings: [creator_info],
       typeSpeed: typer_speed_value,
     }); 
+    
   const locator = () => {
     window.document.location.href = `https://web-developer-is-here.github.io/Calculator/index.html`;
   }
-  
-/*   const blacker_remover = () => {
-    //blacker.id = 'now_not_body_hider';
-    blacker.style.display = 'none';
-    console.log(blacker);
-} */
 
   
-    setInterval(()=>{
+ const interval =  setInterval(()=>{
   if(window.document.querySelector('#creator_para').innerHTML == creator_info){
     creator.innerHTML += `<button id="creator_ok_btn">Ok</button>`;
-    
+    clearInterval(interval);
     let btn = document.querySelector('#creator_ok_btn');
     
     if(btn !== null){
     btn.addEventListener('click',()=>{
       if(creator.style.display !== 'none'){
         locator();
-   /*    blacker_remover(); */
       }
     });
   }
